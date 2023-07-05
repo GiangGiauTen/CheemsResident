33f858bd48fa9f91a644491d3c5af607632ffc82
Neutral P <vuthanhtrung2048@gmail.com>
2023-07-05 16:23:26 +0700

    Fix Household Add

--- api/routers/household.js
+++ api/routers/household.js
@@ -1,26 +1,26 @@
-const express = require('express')
-const { getAllHousehold } = require('../controller/Household/Household.js')
-const { createNewHousehold } = require('../controller/Household/Household.js')
-const router = express.Router()
+const express = require('express');
+const { getAllHousehold } = require('../controller/Household/Household.js');
+const { createNewHousehold } = require('../controller/Household/Household.js');
+const router = express.Router();
 
 module.exports = (connection) => {
 	router.get('/', async (req, res) => {
 		try {
-			const result = await getAllHousehold(connection)
-			res.status(200).json(result)
+			const result = await getAllHousehold(connection);
+			res.status(200).json(result);
 		} catch (error) {
-			console.error('Error fetching data', error)
-			res.status(500).json({ error: 'Internal server error' })
+			console.error('Error fetching data', error);
+			res.status(500).json({ error: 'Internal server error' });
 		}
-	})
+	});
 
 	router.post('/add', async (req, res) => {
 		try {
-			await createNewHousehold(req.body, connection, res)
+			await createNewHousehold(req.body, connection, res);
 		} catch (error) {
-			console.error('Error creating household', error)
+			console.error('Error creating household', error);
 		}
-	})
+	});
 
-	return router
-}
+	return router;
+};
